4f7d66587b9bc258e87794b820b667e70876faed
DouglasHdezT <00004516@uca.edu.sv>
2020-06-24 01:10:59 -0600

    Fix: connection

--- config/database.js
+++ config/database.js
@@ -12,7 +12,7 @@ const uri = `mongodb://${host}:${port}/${database}`;
 const connect = ()=>{
 	console.log(uri);
 	
-    Mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, user: username, pass: password })
+    Mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, user: username, pass: password, authSource: "admin" })
     .then(()=>{
         console.log(`Conection to ${database} successful`);
     })
