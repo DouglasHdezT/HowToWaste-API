4578841b621d9634ba15d46a2332cae3e44f93f4
DouglasHdezT <00004516@uca.edu.sv>
2020-06-25 13:04:53 -0600

    Add: Basic update funtions

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const database = require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var auxRouter = require('./routes/aux');
 
 /**
  * Basic configuration
@@ -34,5 +35,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/aux", auxRouter);
 
 module.exports = app;
