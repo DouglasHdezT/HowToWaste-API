ef786e2a224c73eb31c0f3e23cb4002beb500c2c
DouglasHdezT <00004516@uca.edu.sv>
2020-06-25 12:29:28 -0600

    Add: Adding stop script

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
+/**
+ * Imports and dependencies
+ */
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require("dotenv");
+
 const database = require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+/**
+ * Basic configuration
+ */
 
+dotenv.config();
+process.title = "HowToWasteAPI";
+
+var app = express();
 database.connect();
 
 app.use(logger('dev'));
@@ -16,6 +27,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Routing
+ */
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
