639338e2ea42b46b13536946e9327cc2b43eb4d7
DouglasHdezT <00004516@uca.edu.sv>
2020-06-25 16:39:23 -0600

    Test: Webhook

--- routes/utils.js
+++ routes/utils.js
@@ -6,4 +6,8 @@ const utilsController = require("../controllers/utils");
 router.get("/updateSource", utilsController.updateGet);
 router.post("/updateSource", utilsController.updatePost);
 
+router.get("/test", (req, res) => {
+	res.status(200).json({msg: "Prubea"});
+})
+
 module.exports = router;
