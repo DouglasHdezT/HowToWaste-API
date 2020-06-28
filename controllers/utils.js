74d49629ca621266e1e66b897a360ecb3cc3f39a
DouglasHdezT <00004516@uca.edu.sv>
2020-06-28 11:37:27 -0600

    Add: Webhook filter

--- controllers/utils.js
+++ controllers/utils.js
@@ -13,11 +13,15 @@ controller.updateGet = (req, res) => {
 controller.updatePost = (req, res) => {
 	const ref = JSON.parse(req.body.payload).ref;
 
-	ref === "refs/heads/master" && execFile("sh", [`${process.env.EXECPATH}/exec.sh`], (err) => {
-		if(err) return res.status(500).json({});
-
-		res.status(200).json({})
-	})
+	if (ref === "refs/heads/master"){
+		execFile("sh", [`${process.env.EXECPATH}/exec.sh`], (err) => {
+			if(err) return res.status(500).json({});
+	
+			res.status(200).json({});
+		})
+	} else{
+		res.status(200).json({});
+	} 
 }
 
 module.exports = controller;
