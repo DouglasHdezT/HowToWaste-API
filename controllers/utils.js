b2fd667337f5edd72ffa2a9c9c49c6f3e1e3b534
DouglasHdezT <00004516@uca.edu.sv>
2020-06-28 11:28:19 -0600

    Test: ref WeebHook

--- controllers/utils.js
+++ controllers/utils.js
@@ -11,7 +11,7 @@ controller.updateGet = (req, res) => {
 }
 
 controller.updatePost = (req, res) => {
-	console.log(req.payload.ref);
+	console.log(Object.keys(req));
 
 	execFile("sh", [`${process.env.EXECPATH}/exec.sh`], (err) => {
 		if(err) return res.status(500).json({});
