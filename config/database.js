8e8ebe08225f5c0f5427e08d8a16ce18fabeba83
DouglasHdezT <00004516@uca.edu.sv>
2020-06-29 16:23:56 -0600

    Fix: authsource

--- config/database.js
+++ config/database.js
@@ -2,8 +2,9 @@ const Mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config()
 
-const username = process.env.DBUSER || "root";
-const password = process.env.DBPASS || "root";
+const username = process.env.DBUSER;
+const password = process.env.DBPASS;
+const authSource = process.env.AUTHSOURCE;
 
 const database = "HowToWaste";
 const host = process.env.DBHOST || "localhost";
@@ -12,7 +13,7 @@ const port = process.env.DBPORT || "27017";
 const uri = `mongodb://${host}:${port}/${database}`;
 
 const connect = ()=>{
-    Mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, user: username, pass: password, authSource: "admin" })
+    Mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, user: username, pass: password, authSource: authSource })
     .then(()=>{
         console.log(`Conection to ${database} successful`);
     })
