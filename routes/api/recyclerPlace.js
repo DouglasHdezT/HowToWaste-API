d62e740d4040410c5e1f9ed4d52ceb10066dcd78
DouglasHdezT <00004516@uca.edu.sv>
2020-06-28 22:10:16 -0600

    Add: Recycler Places route and basic dump data

---
+++ routes/api/recyclerPlace.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const recyclerPlaceController = require("../../controllers/api/recyclerPlaces");
+
+//router.get("/dumpData", recyclerPlaceController.dumpRecyclerPlaces);
+
+module.exports = router; 
