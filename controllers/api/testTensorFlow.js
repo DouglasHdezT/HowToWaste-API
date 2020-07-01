fa84780bb9e4b4d6270289ccd033bc2f27637469
DouglasHdezT <00004516@uca.edu.sv>
2020-07-01 17:24:08 -0600

    Test: Tesor Prediction

--- controllers/api/testTensorFlow.js
+++ controllers/api/testTensorFlow.js
@@ -1,14 +1,22 @@
 const tf = require('@tensorflow/tfjs');
+const tfNode = require('@tensorflow/tfjs-node');
+const fs = require('fs');
 const mobilenet = require('@tensorflow-models/mobilenet');
+
 
 const controller = {};
 
 controller.test = async (req, res) => {
-	console.log(req);
-
+	const { file } = req;
+	
 	try {
-		const model = await mobilenet.load();
-		res.status(200).json({message: "Se cargo"});
+		const model = await mobilenet.load();		
+		const buffer = fs.readFileSync(file.path);
+		const tfImage = tfNode.node.decodeImage(buffer);
+
+		const predictions = await model.classify(tfImage);
+
+		res.status(200).json({message: predictions});
 	}catch(e){
 		res.status(500).json({message: e});
 	}
--- routes/api/testTensorFlow.js
+++ routes/api/testTensorFlow.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const multer = require('multer');
+const upload = multer({dest: '../../public/uploads'});
 
 const tensorController = require("../../controllers/api/testTensorFlow");
 
-router.post("/test", tensorController.test);
+router.post("/test", upload.single('image') ,tensorController.test);
 
 module.exports = router;
