4cf013143ed5fa5f1cfe25d634a7d4ec2e2259a7
DouglasHdezT <00004516@uca.edu.sv>
2020-07-03 18:05:05 -0600

    Add: Get all tensors controller

--- controllers/api/objectClassifier.js
+++ controllers/api/objectClassifier.js
@@ -29,14 +29,24 @@ controller.saveObject = async (req, res) => {
 			const tensorDoc = await tensorStringDoc.save();
 			console.log(`Saved Tensor: ${tensorDoc}`);
 			
-			return res.status(200).json({message: "Model saved Successfully"})
+			return res.status(200).json({message: "Model saved Successfully"});
 		} catch (error) {
 			console.log(error);
-			return res.status(500).json({message: "Internal server error"})
+			return res.status(500).json({message: "Internal server error"});
 		}
 	}else{
-		return res.status(400).json({message: "Bad Request!"})
+		return res.status(400).json({message: "Bad Request!"});
 	}
+}
+
+controller.getAllTensors = (req, res) => {
+	TensorStringModel.find({}, (err, docs) => {
+		if(err) return res.status(500).json({message: "Internal server error"});
+
+		if(!docs) return res.status(404).json({message: "Not Found"});
+		
+		return res.status(200).json({tensors: docs});
+	});
 }
 
 module.exports = controller;
