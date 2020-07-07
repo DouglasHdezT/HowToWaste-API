3d480cc8d1c3d0716a00e7866c521aca0213deeb
DouglasHdezT <00004516@uca.edu.sv>
2020-07-06 19:16:43 -0600

    Fix: unsafety routes

--- routes/api/material.js
+++ routes/api/material.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const materialController = require("../../controllers/api/material");
 
-router.get("/dumpAll", materialController.dumpAll);
+//router.get("/dumpAll", materialController.dumpAll);
 router.get("/getAll", materialController.getAll);
 router.get("/one/:id", materialController.getOneByID);
 router.post("/item", materialController.insert);
--- routes/api/objectClassifier.js
+++ routes/api/objectClassifier.js
@@ -6,7 +6,7 @@ const upload = multer({dest: '../../public/uploads'});
 const objectClassifierController = require('../../controllers/api/objectClassifier');
 
 router.get("/getAll", objectClassifierController.getAllTensors);
-router.get("/dropAll", objectClassifierController.dropCollection);
+//router.get("/dropAll", objectClassifierController.dropCollection);
 router.post("/saveModel", upload.single("image"), objectClassifierController.saveObject);
 router.post("/classify", upload.single("image"), objectClassifierController.classifyObject);
 
